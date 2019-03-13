240450267f9d68c0601a4850b688eba3bf13f3eb
Cristina Rojas <telematicacristina@gmail.com>
2019-03-12 22:32:37 -0700

    Adding for to handle sign up

--- src/shared/components/layout/userForm/userForm.jsx
+++ src/shared/components/layout/userForm/userForm.jsx
@@ -5,29 +5,86 @@ import React, { Fragment, Component } from 'react';
 import styles from './userForm.scss';
 
 class userForm extends Component  {
+  constructor() {
+    super();
+
+    this.state = {
+      name: '',
+      email: '',
+      password: '',
+      signUp: true,
+      payment: false
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    if (event.target.id === "name") {
+      this.setState({
+        name: event.target.value
+      });
+    } else if (event.target.id === "email") {
+      this.setState({
+        email: event.target.value
+      });
+    } else {
+      this.setState({
+        password: event.target.value
+      });
+    }
+  }
+
+  handleSubmit(event) {
+    console.log('submited', this.state);
+    this.setState({
+      signUp: !this.state.signUp,
+      payment: !this.state.payment
+    });
+    event.preventDefault();
+  }
+
+
   render() {
     return (
       <section className={styles.containerForm}>
         <Fragment>
           <div className={styles.userForm}>
-            <p className={styles.signIn}>Sign In</p>
-            <p className={styles.signUp}>Sign Up</p>
+            <p
+              className={this.state.signUp ? `${styles.signUp} ${styles.active}` : `${styles.signUp} ${styles.inactive}` }
+            >Sign Up</p>
+            <p
+              className={this.state.payment ? `${styles.payment} ${styles.active}` : `${styles.payment} ${styles.inactive}` }
+              >Payment</p>
 
-            <form className={styles.form}>
+            <form className={styles.form} onSubmit={this.handleSubmit}>
               <input
+                id='name'
                 name='name'
                 className={styles.data}
-                placeholder="Name" />
+                placeholder="Name"
+                value={this.state.name}
+                onChange={this.handleChange}
+               />
 
               <input
+                id="email"
                 name='email'
                 className={styles.data}
-                placeholder="Email" />
+                placeholder="Email"
+                value={this.state.email}
+                onChange={this.handleChange}
+              />
 
                 <input
+                  id="password"
                   name='password'
                   className={styles.data}
-                  placeholder="Password" />
+                  placeholder="Password"
+                  value={this.state.password}
+                  onChange={this.handleChange}
+                />
 
               <input
                 type="submit"
